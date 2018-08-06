c84e0247f6d9853834985ef514f12fbb7f5f73c4
skatolka <skatolka@DIN73003000.corp.capgemini.com>
2018-08-06 12:10:55 +0530

    Sales Class added

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -3,8 +3,10 @@ package org.cap.demo;
 public class MainClass {
 
 	public static void main(String[] args) {
+		SalesClass class1=new SalesClass();
 		System.out.println("Hello World!!");
 		greet();
+		class1.show();
 	}
 	
 	public static void greet() {
---
+++ Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
@@ -0,0 +1,12 @@
+package org.cap.demo;
+
+public class SalesClass {
+	
+	int[] array= {1,2,3,-55,87};
+
+	public void show() {
+		for(int num:array) {
+			System.out.println(num);
+		}
+	}
+}
