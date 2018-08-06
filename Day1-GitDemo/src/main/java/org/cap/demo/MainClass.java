e3fd3dee193e771e28303aebc9f4fd835d68ee36
skatolka <skatolka@DIN73003000.corp.capgemini.com>
2018-08-06 10:52:52 +0530

    Commit once again

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -4,9 +4,13 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
-
+		greet();
 	}
 	
-	
+	public static void greet() {
+		System.out.println("Good Morning!!");
+		System.out.println("Welcome to Git Hub.");
+		System.out.println("This is your first project.");
+	}
 
 }
